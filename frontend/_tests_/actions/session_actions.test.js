49aa1fe7f1aae957a4c141a3044211f85f3f4a3c
isaiah3031 <isaiah.whitfield@gmail.com>
2020-12-02 11:25:29 -0500

    Session actions and reducer complete.

--- frontend/_tests_/actions/session_actions.test.js
+++ frontend/_tests_/actions/session_actions.test.js
@@ -1,3 +1,9 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import fetchMock from 'fetch-mock'
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
 import {
   login, 
   logout, 
@@ -6,3 +12,62 @@ import {
   logoutCurrentUser, 
   receiveErrors
 } from '../../actions/session_actions'
+
+import * as types from '../../constants/action_types'
+import { config } from 'fetch-mock'
+
+// Regular Actions
+
+describe('regular actions', () => {
+  const user = { 1: { id: 2, username: 'isaiah30' }}
+  it('login action should create an action to log in a user', () => {
+    const expectedAction = {
+      type: types.RECEIVE_CURRENT_USER,
+      user
+    }
+    expect(receiveCurrentUser(user)).toMatchObject(expectedAction)
+  })
+
+  it('signup action should create an action to create a user', () => {
+    const expectedAction = {
+      type: types.LOGOUT_CURRENT_USER,
+    }
+    expect(logoutCurrentUser()).toMatchObject(expectedAction)
+  })
+
+  it('error action should create an action to recieve errors', () => {
+    const errors = { error: 'Incorrect Credentials.'}
+    const expectedAction = {
+      type: types.RECEIVE_ERRORS,
+      errors
+    }
+    expect(receiveErrors(errors)).toMatchObject(expectedAction)
+  })
+})
+
+// Thunk Actions
+
+// describe('async actions', () => {
+//   afterEach(() => {
+//     fetchMock.restore()
+//   })
+
+//   it('creates login user success when fetching user has been done', () => {
+//     const user = { 1: { id: 1, username: 'isaiah30' } }
+
+//     fetchMock.getOnce('/api/users', {
+//       body: { user: { 1: {username: 'isaiah30'} } },
+//       headers: { 'content-type': 'application/json' },
+//       })
+
+//     const expectedActions = [{ 
+//       type: types.RECEIVE_CURRENT_USER, 
+//       body: { user: user }
+//     }]
+//     const store = mockStore({ user: {} })
+//     console.log(store.getActions())
+//     return expect(fetchMock).toHaveFetched('/api/users', 'data')
+//   })
+// })
+
+    
---
+++ frontend/_tests_/reducers/session_reducer.test.js
@@ -0,0 +1,22 @@
+import SessionReducer from '../../reducers/session_reducer'
+import * as types from '../../constants/action_types'
+
+describe('session reducer', () => {
+  it('should return the initial state when passed an unfamiliar type', () => {
+    expect(SessionReducer({}, { type: undefined })).toEqual({})
+  })
+
+  it('should handle RECEIVE_CURRENT_USER', () => {
+    expect(SessionReducer({}, { 
+        type: types.RECEIVE_CURRENT_USER,
+        user: { 1: { username: 'isaiah30' } }
+      }
+    )).toEqual({ 1: {username: 'isaiah30' } })
+  })
+
+  it('should handle LOGOUT_CURRENT_USER', () => {
+    expect(SessionReducer({ 17: { username: 'isaiah30' } }, {
+      type: types.LOGOUT_CURRENT_USER
+    })).toEqual({})
+  })
+})
