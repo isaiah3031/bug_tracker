cc351129731acb2f76ea4412bcf4774c66d778d5
isaiah3031 <isaiah.whitfield@gmail.com>
2021-01-03 08:51:44 -0500

    Cleaned the story list component by moving logic out of the render method.

--- frontend/components/story/story_list.jsx
+++ frontend/components/story/story_list.jsx
@@ -46,7 +46,7 @@ class StoryList extends React.Component {
     return {icebox: icebox, backlog: backlog, current: current, finished: finished}
   }
 
-  // returns an array of sorted arrays
+  // returns an array of stories sorted by their priority
   sortStoriesByPriority() {
     return Object.values(this.props.stories).sort((storya, storyb) => 
       parseFloat(storya.priority) - parseFloat(storyb.priority)
@@ -54,16 +54,17 @@ class StoryList extends React.Component {
   }
 
   // returns start, finished or an empty string depending on string passed to it
-  returnAppropriateText(iteration) {
-    switch(iteration) {
-      case 'current':
-        return 'iteration'
-      case 'backlog':
-        return 'start'
-      default:
-        return ''
-    }
+  toggleDescription(story) {
+    if (this.state.selectedStory == story.id) { 
+      return story.description 
+    } else {
+      return `${story.description.substr(0, 60)}...`
+    }  
   }
+
+  // Loops through stories of each iteration to expose a single story instance. 
+  // [iteration1, iteration2, iteration3, iteration4] => 
+  // [story1, story2, story3...]
   render() {
     if (Object.values(this.props.stories).length == 0 ||
         this.props.stories.id) {
@@ -72,26 +73,24 @@ class StoryList extends React.Component {
     let sortedStories = this.sortStoriesByIteration(this.sortStoriesByPriority())
 
     return (
-      // Loops through stories of each iteration to expose a single story instance. 
-      // Array of Iterations => Iterations Array => story object
-        <div className='iterations'>
-          {Object.keys(sortedStories).map(iteration => 
-              <div className='story-list'>
-                <h2>{iteration}</h2>
-              {sortedStories[iteration].map(story => {
-                return <li className='story-component' key={story.id}>
-                  <p className='description'
-                    onClick={() => this.setSelectedStory(story.id)}>
-                    {this.state.selectedStory == story.id ? story.description : `${story.description.substr(0, 60)}...`}
-                  </p>
-                  <QuickFormContainer story={story}/>
-                  <StoryDetail selectedStory={this.state.selectedStory} story={story} />
-                </li>
-                }
-              )}
-            </div>
-        )}
-      </div>
+      <div className='iterations'>
+        {Object.keys(sortedStories).map(iteration => 
+            <div className='story-list'>
+            <h2>{iteration}</h2>
+            {sortedStories[iteration].map(story => 
+              <li className='story-component' key={story.id}>
+                <p className='description'
+                  onClick={() => this.setSelectedStory(story.id)}>
+                  {this.toggleDescription(story)}
+                </p>
+                <QuickFormContainer story={story}/>
+                <StoryDetail selectedStory={this.state.selectedStory} story={story} />
+              </li>
+            )
+          }
+        </div>
+      )}
+    </div>
   )}
 }
 
