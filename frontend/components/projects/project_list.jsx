5741f2b57d2c02857de671b36b5f4d44c500200c
isaiah3031 <isaiah.whitfield@gmail.com>
2021-01-24 19:12:24 -0500

    The project list is now only visible when the use hovers over 'Choose a Project'

--- frontend/components/projects/project_list.jsx
+++ frontend/components/projects/project_list.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom'
 import ToggleNewForm from '../story/toggle_new_form'
 import LoadingIcon from '../loading_icon'
 
 const ProjectList = (props) => {
+  const [hovered, setHovered] = useState(false);
+  const toggleHover = () => setHovered(!hovered);
+  
   useEffect(() => {
     props.fetchProjects();
     }, [])
@@ -17,15 +20,38 @@ const ProjectList = (props) => {
   if (props.loading) { return <LoadingIcon />; }
 
   return (
-    <ul className='project-list'>
-      <h2>Projects</h2>
-      {Object.values(projects).map(project => {
-        return <li key={project.id} onClick={() => showProject(project.id)}>
-          {project.name}
-        </li>
-      }
-    )}
-    </ul>
+    <>
+    <div className='project-bar'
+      onMouseEnter={toggleHover}
+      onMouseLeave={toggleHover}>
+      <h2>Choose a Project</h2>
+      <ul className={hovered ? 'project-list' : 'hidden'}t>
+        {Object.values(projects).map(project => {
+          return <li key={project.id} onClick={() => showProject(project.id)}>
+            {project.name}
+          </li>
+          }
+        )}
+      </ul>
+    </div>
+    <div className='welcome-msg-container'>
+      <div className='welcome-msg'>
+        <h1>Welcome to BugHunter!</h1>
+        <p>
+          You must Login or Sign up before you can view bugs
+        </p>
+        <p>  
+          Choose or create a project from the dropdown in the top left corner.
+        </p>
+        <p>
+              You can only edit the bugs on  aproject you have created or that you have admin rights over.
+        </p>
+        <p>
+          Once viewing bugs for a project, you can drag and drop them to change their priority.
+        </p>
+      </div>
+      </div>
+    </>
   )
 }
 
