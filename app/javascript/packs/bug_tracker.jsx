871e944d9e2e6b700188e5edda0d038cf5eabf17
isaiah3031 <isaiah.whitfield@gmail.com>
2021-02-21 18:28:25 -0500

    Removing dispatch funtion and getState from the console.

--- app/javascript/packs/bug_tracker.jsx
+++ app/javascript/packs/bug_tracker.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { store } from '../../../frontend/store/store'
 import Root from '../../../frontend/components/root'
-import {logout} from '../../../frontend/actions/session_actions'
-window.logout = logout()
+
 // You need to make the logout function publicly availible because of the
 // heroku loop 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root')
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
   ReactDOM.render(<Root store={store}/>, root)
 })
