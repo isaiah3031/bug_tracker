27874ac8c7fed3d67f0e7e43c8088bc7ab23a9f7
isaiah3031 <isaiah.whitfield@gmail.com>
2021-02-21 17:59:19 -0500

    Production state is stuck between logged in and logged out. This will make the logout function availible in the console so I can fix it.

--- app/javascript/packs/bug_tracker.jsx
+++ app/javascript/packs/bug_tracker.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { store } from '../../../frontend/store/store'
 import Root from '../../../frontend/components/root'
+import {logout} from '../../../frontend/actions/session_actions'
+window.logout = logout
+// You need to make the logout function publicly availible because of the
+// heroku loop 
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root')
